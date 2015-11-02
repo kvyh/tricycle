5b1d653f20d408b998dc49ef72ad6edf321e926e
jlurie <luriejcc@gmail.com>
2015-11-02 15:32:28 -0800

    Added arguments for primary/secondary eclipse depths/widths.
    The primary and secondary eclipse widths and depths are now attributes
    of the LightCurvey class. However, ModelBinary._make_lc only uses the
    primary eclipse.

--- binaries.py
+++ binaries.py
@@ -11,14 +11,14 @@ class ModelBinary(LightCurve):
 
     Parameters
     ----------
-    p_rot1 : float
-        Rotation period of star 1 in days.
-    p_rot2 : float
-        Rotation period of star 2 in days.
-    amp_1 : float
-        Relative spot amplitude of star 1.
-    amp 2 : float
-        Relative spot amplitude of star 2.
+    p_rot : float
+        Rotation period of star occulted during primary eclipse.
+    s_rot : float
+        Rotation period of star occulted during secondary eclipse.
+    p_amp : float
+        Relative spot amplitude of star occulted during primary eclipse.
+    s_amp : float
+        Relative spot amplitude of star occulted during secondary eclipse.
     length : float
         Length of desired light curve in days.
     sig: float
@@ -27,19 +27,22 @@ class ModelBinary(LightCurve):
         Default is for short cadence. Set to False for long cadence.
 
     """
-    def __init__(self, p_orb=7.07, t_0=0.0, e_dur=0.27, depth=0.5, p_rot1=1.51,
-                 p_rot2=0.80, amp_1=0.03, amp_2=0.02, length=100.0, sig=0.02,
-                 sc=True):
+    def __init__(self, p_orb=7.07, t_0=0.0, p_depth=0.23, s_depth=0.17,
+                 p_width=0.02, s_width=0.02, p_rot=1.51, s_rot=0.80,
+                 p_amp=0.03, s_amp=0.02, length=100.0, sig=0.02, sc=True):
 
-        time, flux, err = self.__make_lc(p_orb, t_0, e_dur, depth, p_rot1,
-                                         p_rot2, amp_1, amp_2, length, sig, sc)
+        time, flux, err = self._make_lc(p_orb, t_0, p_width * p_orb, p_depth,
+                                        p_rot, s_rot, p_amp, s_amp, length,
+                                        sig, sc)
 
-        super(ModelBinary, self).__init__(time, flux, err, p_orb, t_0, e_dur,
-                                          depth)
+        super(ModelBinary, self).__init__(time, flux, err, p_orb, t_0, p_depth,
+                                          s_depth, p_width, s_width)
 
+    # TODO: Light curve model should account for both primary and secondary
+    # eclipse properties.
     @staticmethod
-    def __make_lc(p_orb, t_0, e_dur, depth, p_rot1, p_rot2, amp_1, amp_2,
-                  length, sig, sc):
+    def _make_lc(p_orb, t_0, e_dur, depth, p_rot1, p_rot2, amp_1, amp_2,
+                 length, sig, sc):
         """
         Make a light curve.
 
