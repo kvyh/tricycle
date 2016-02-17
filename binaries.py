93e6d5bcb35a7111adec0df3840736154228c16f
kvyh <Karlvyh@hotmail.com>
2016-02-17 15:10:00 -0800

    Update binaries.py
    to match John's code

--- binaries.py
+++ binaries.py
@@ -38,8 +38,9 @@ class ModelBinary(LightCurve):
                                         p_rot, s_rot, p_amp, s_amp, length,
                                         sig, sc)
 
-        super(ModelBinary, self).__init__(time, flux, err, p_orb, t_0, p_depth,
-                                          s_depth, p_width, s_width, -999)
+        super(ModelBinary, self).__init__(time, flux, err, None, p_orb, t_0,
++                                          p_depth, s_depth, p_width, s_width,
++                                          -999)
 
     # TODO: Light curve model should account for both primary and secondary
     # eclipse properties.
@@ -103,7 +104,7 @@ class RealBinary(LightCurve):
         sdepth=dfnew.sdepth.values[0]
         sep=dfnew.sep.values[0]
         time, flux, fluxerr, cadence, quarter, quality = data.loadlc_db(kic)
-        super(RealBinary, self).__init__(time, flux, fluxerr, period, bjd0,
-                                         pdepth, sdepth, pwidth, swidth, sep,
-                                         kic)
+         super(RealBinary, self).__init__(time, flux, fluxerr, quarter, period,
++                                         bjd0, pdepth, sdepth, pwidth, swidth,
++                                         sep, kic)
         
